f5add9e390bbcba7cbeea7e41ddeff8af6222468
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-07-29 07:54:11 +0530

    now the first practical of compiler design is working correctly

--- Compiler Design/Practical1.cpp
+++ Compiler Design/Practical1.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> findKeywords(string s,vector<string> arr, vector<string> keywords)
+vector<string> findKeywords(string s,vector<string> arr)
 {
+    vector<string> keywords;
+
     for(int i=0; i<arr.size(); i++)
     {
         int size = arr[i].size();
@@ -15,10 +17,14 @@ vector<string> findKeywords(string s,vector<string> arr, vector<string> keywords
             }
         }
     }
+
+    return keywords;
 }
 
-vector<string> findVariables(string s, vector<string> arr, vector<string> variables)
+vector<string> findVariables(string s, vector<string> arr)
 {
+    vector<string> variables;
+
     for(int i=0; i<arr.size(); i++)
     {
         int size = arr[i].size();
@@ -31,47 +37,88 @@ vector<string> findVariables(string s, vector<string> arr, vector<string> variab
             }
         }
     }
+
+    return variables;
 }
 
-vector<string> findOperators(string s, vector<string> arr, vector<string> operators)
+vector<char> findOperators(string s, vector<char> arr)
 {
-    for(int i=0; i<arr.size(); i++)
+    vector<char> operators;
+
+    for(int i=0; i<s.size(); i++)
     {
-        int size = arr[i].size();
-    
-        for(int j=0; j<=s.size()-size; j++)
+        for(int j=0; j<arr.size(); j++)
         {
-            if(s.substr(j,size)==arr[i])
+            if(arr[j] == s[i])
             {
-                operators.push_back(arr[i]);
+                operators.push_back(arr[j]);
             }
         }
     }
+
+    return operators;
 }
 
 
 int main()
 {
-    vector<string> arr = {"int", "float", "double", "string", "char", "break", "catch", "for", "while", "class", "return", "goto", "a", "b", "c", "var1", "temp", "arr", "s", "+", "-", "/", "&", "*", "%", "!", "="};
-    vector<string> keywords;
-    vector<string> variables;
-    vector<string> operators;
+    vector<string> keywordsList = {"int", "float", "double", "string", "char", "break", "catch", "for", "while", "class", "return", "goto", };
+    vector<string> variablesList = {"var1", "temp", "arr", "name"};
+    vector<char> operatorsList = {'+', '-', '/', '&', '*', '%', '!', '='};
 
     string s;
     cout<<"Enter string: ";
     cin>>s;
 
+    vector<string> keywords = findKeywords(s, keywordsList);
+    vector<string> variables = findVariables(s, variablesList);
+    vector<char> operators = findOperators(s, operatorsList);
 
     if(keywords.size()!=0)
     {
+        cout<<"Keywords are: ";
+
         for(int i=0; i<keywords.size(); i++)
         {
-            cout<<keywords[i]<<endl;
+            cout<<keywords[i]<<" ";
         }
+        cout<<endl;
     }
     else
     {
         cout<<"Not Found"<<endl;
     }
+
+
+    if(variables.size()!=0)
+    {
+        cout<<"Variables are: ";
+
+        for(int i=0; i<variables.size(); i++)
+        {
+            cout<<variables[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else
+    {
+        cout<<"Not Found"<<endl;
+    }
+
+    if(operators.size()!=0)
+    {
+        cout<<"Operators are: ";
+
+        for(int i=0; i<operators.size(); i++)
+        {
+            cout<<operators[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else
+    {
+        cout<<"Not Found"<<endl;
+    }
+
     return 0;
 }
