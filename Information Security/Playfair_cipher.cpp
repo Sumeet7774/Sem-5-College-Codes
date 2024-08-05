9636b48fdcdd152c6f4a1c01a8f250bf197aa991
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-05 14:05:15 +0530

    made some programs in python lecture

--- Information Security/Playfair_cipher.cpp
+++ Information Security/Playfair_cipher.cpp
@@ -1,18 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+pair<char,char> splitString(vector<char> alphabets, string plaintext)
+{
+}
+
+
+
+
 int main()
 {
     int rows=5;
     int cols=5;
 
     int matrix[rows][cols];
+    vector<char> alphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
     string plaintext;
     cout<<"Enter plaintext: "; 
     getline(cin,plaintext);
 
-        
+    set<char> uniqueValues;
+
+    for(int i=0; i<plaintext.size(); i++)
+    {
+        uniqueValues.insert(plaintext[i]);
+    }
+
+
+
+
+
+
 
 
 
